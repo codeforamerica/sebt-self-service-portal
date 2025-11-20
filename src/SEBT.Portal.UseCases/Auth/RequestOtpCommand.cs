366e6dbcb57dc824094643cbf471fd0ad1fa563f
jhernandez-cfa <jhernandez@codeforamerica.org>
2025-11-20 16:43:45 -0700

    Refactor OTP handling: remove RequestOtpCommandResult, implement IOtpRepository, and enhance RequestOtpCommandHandler with OTP generation and email sending logic

--- src/SEBT.Portal.UseCases/Auth/RequestOtpCommand.cs
+++ src/SEBT.Portal.UseCases/Auth/RequestOtpCommand.cs
@@ -1,12 +1,11 @@
 using System.ComponentModel.DataAnnotations;
-using SEBT.Portal.Core.Models.Results;
 using SEBT.Portal.Kernel;
 
 namespace SEBT.Portal.UseCases.Auth;
 /// <summary>
 /// Command to request a one-time password (OTP).
 /// </summary>
-public class RequestOtpCommand : ICommand<RequestOtpCommandResult>
+public class RequestOtpCommand : ICommand
 {
     /// <summary>
     /// The email address to which the OTP will be sent.
--- src/SEBT.Portal.UseCases/Auth/RequestOtpCommandHandler.cs
+++ src/SEBT.Portal.UseCases/Auth/RequestOtpCommandHandler.cs
@@ -1,21 +1,52 @@
-using SEBT.Portal.Core.Models.Results;
+using Microsoft.Extensions.Logging;
+using Sebt.Portal.Core.Models.Auth;
+using SEBT.Portal.Core.Repositories;
+using SEBT.Portal.Core.Services;
 using SEBT.Portal.Kernel;
 using SEBT.Portal.Kernel.Results;
 
 namespace SEBT.Portal.UseCases.Auth
 {
-    public class RequestOtpCommandHandler(IValidator<RequestOtpCommand> validator) : ICommandHandler<RequestOtpCommand, RequestOtpCommandResult>
+    public class RequestOtpCommandHandler(
+        IValidator<RequestOtpCommand> validator,
+        IOtpGenerator otpGenerator,
+        IEmailSender emailService,
+        IOtpRepository otpRepository,
+        ILogger<RequestOtpCommandHandler> logger)
+        : ICommandHandler<RequestOtpCommand>
     {
-        public async Task<Result<RequestOtpCommandResult>> Handle(RequestOtpCommand command, CancellationToken cancellationToken)
+        public async Task<Result> Handle(RequestOtpCommand command, CancellationToken cancellationToken)
         {
             var validationResult = await validator.Validate(command, cancellationToken);
 
             if (validationResult is ValidationFailedResult validationFailedResult)
             {
-                return Result<RequestOtpCommandResult>.ValidationFailed(validationFailedResult.Errors);
+                return Result.ValidationFailed(validationFailedResult.Errors);
             }
 
-            return Result<RequestOtpCommandResult>.PreconditionFailed(PreconditionFailedReason.NotFound, "Not implemented");
+            try
+            {
+                var otp = new OtpCode(otpGenerator.GenerateOtp(), command.Email);
+                await otpRepository.SaveOtpCodeAsync(otp);
+            }
+            catch (System.Exception e)
+            {
+                logger.LogError(e, "An error occurred while attemting to persist the OTP request for email {Email}", command.Email);
+                return Result.DependencyFailed(DependencyFailedReason.Timeout,
+                    $"An error occurred while processing the OTP request");
+            }
+
+            try
+            {
+                await emailService.SendEmailAsync("", "", "");
+            }
+            catch (System.Exception e)
+            {
+                logger.LogError(e, "An error occurred while sending the OTP email for email {Email}", command.Email);
+                return Result.DependencyFailed(DependencyFailedReason.Timeout,
+                    $"An error occurred while processing the OTP request");
+            }
+            return Result.Success();
         }
     }
 }
