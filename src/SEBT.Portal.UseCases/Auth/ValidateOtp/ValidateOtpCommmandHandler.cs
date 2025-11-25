880c5f9201405511963cfa568ecf0cf943ea8459
jhernandez-cfa <jhernandez@codeforamerica.org>
2025-11-25 15:30:58 -0700

    Implement ValidateOtpCommandHandler logic and enhance unit tests for OTP validation scenarios

--- src/SEBT.Portal.UseCases/Auth/ValidateOtp/ValidateOtpCommmandHandler.cs
+++ src/SEBT.Portal.UseCases/Auth/ValidateOtp/ValidateOtpCommmandHandler.cs
@@ -1,6 +1,7 @@
 using Microsoft.Extensions.Logging;
 using SEBT.Portal.Core.Repositories;
 using SEBT.Portal.Kernel;
+using SEBT.Portal.Kernel.Results;
 
 namespace SEBT.Portal.UseCases.Auth
 {
@@ -10,9 +11,26 @@ namespace SEBT.Portal.UseCases.Auth
         ILogger<ValidateOtpCommandHandler> logger)
         : ICommandHandler<ValidateOtpCommand>
     {
-        public Task<Result> Handle(ValidateOtpCommand command, CancellationToken cancellationToken = default)
+        public async Task<Result> Handle(ValidateOtpCommand command, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            var validationResult = await validator.Validate(command, cancellationToken);
+
+            if (validationResult is ValidationFailedResult validationFailedResult)
+            {
+                return Result.ValidationFailed(validationFailedResult.Errors);
+            }
+
+            var otp = await otpRepository.GetOtpCodeByEmailAsync(command.Email);
+
+            if (otp is null || otp.IsCodeValid(command.Otp) == false)
+            {
+                return Result.ValidationFailed(new[]
+                {
+                    new ValidationError("Otp", "The provided OTP is invalid or has expired.")
+                });
+            }
+
+            return new SuccessResult();
         }
     }
 
