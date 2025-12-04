6c6600dde1eefd944e7037f6ed62b0bd9a29d6af
jhernandez-cfa <jhernandez@codeforamerica.org>
2025-12-04 11:24:48 -0700

    Add portal infrastructure services, repositories, and app settings to Program.cs

--- src/SEBT.Portal.Api/Program.cs
+++ src/SEBT.Portal.Api/Program.cs
@@ -18,7 +18,9 @@ builder.Services.AddSwaggerGen();
 
 // Adds use cases (i.e., query and command handlers) for portal business logic
 builder.Services.AddUseCases();
-builder.Services.AddPortalInfrastructureServices();
+builder.Services.AddPortalInfrastructureServices(); 
+builder.Services.AddPortalInfrastructureRepositories();
+builder.Services.AddPortalInfrastructureAppSettings();
 
 var app = builder.Build();
 
