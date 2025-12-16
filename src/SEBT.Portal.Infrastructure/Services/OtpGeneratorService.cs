a8e2bb4e3ccabb07135ea16dbf36e3147f226042
Michael Walsh <noxiousthing@gmail.com>
2025-12-16 09:35:38 -0800

    DC-58 Update: Use GetInt32 from System.Security.Cryptography

--- src/SEBT.Portal.Infrastructure/Services/OtpGeneratorService.cs
+++ src/SEBT.Portal.Infrastructure/Services/OtpGeneratorService.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography;
 using SEBT.Portal.Core.Services;
 
 namespace SEBT.Portal.Infrastructure.Services
@@ -8,18 +9,13 @@ namespace SEBT.Portal.Infrastructure.Services
     public class OtpGeneratorService : IOtpGeneratorService
     {
         /// <summary>
-        /// A shared instance of the Random class used for generating random numbers.
-        /// </summary>
-        private readonly Random _random = Random.Shared;
-
-        /// <summary>
         /// Generates a 6-digit numeric One-Time Password (OTP).
         /// </summary>
         /// <returns>A string representation of a randomly generated 6-digit numeric code.</returns>
         public string GenerateOtp()
         {
             // Simple OTP generation logic (6-digit numeric code)
-            return _random.Next(100000, 1000000).ToString();
+            return RandomNumberGenerator.GetInt32(100000, 1000000).ToString();
         }
     }
 }
