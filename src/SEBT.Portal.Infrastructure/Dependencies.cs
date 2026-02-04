814ffeb9f2a37110b75a2bac50b637a30e3b2935
Michael Walsh <noxiousthing@gmail.com>
2026-02-04 13:05:08 -0800

    DC-97 Fix: DataSeeder missing identifierHasher (post-rebase)

--- src/SEBT.Portal.Infrastructure/Dependencies.cs
+++ src/SEBT.Portal.Infrastructure/Dependencies.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using SEBT.Portal.Core.AppSettings;
 using SEBT.Portal.Core.Repositories;
 using SEBT.Portal.Core.Services;
