ffc9bb6fb73946ae9e3452b65716572ec2610a5a
spokenbird <agonzalez@codeforamerica.org>
2026-02-10 13:25:26 -0800

    Add logging module

--- tofu/config/dev-dc/main.tf
+++ tofu/config/dev-dc/main.tf
@@ -6,3 +6,13 @@ terraform {
     region         = "us-east-1"
   }
 }
+
+# Create an S3 bucket and KMS key for logging.                                                                                                    
+module "logging" {
+  source = "github.com/codeforamerica/tofu-modules-aws-logging?ref=2.1.2"
+
+  project     = "sebt-portal"
+  environment = "dev"
+
+  log_groups_to_datadog = false
+}
