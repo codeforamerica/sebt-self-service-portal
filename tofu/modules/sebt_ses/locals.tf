12e63e9aa4c9d3313869b3533b5bda3f0b4c79b8
spokenbird <agonzalez@codeforamerica.org>
2026-02-12 12:51:54 -0800

    Remove unused declarations from SES module

--- tofu/modules/sebt_ses/locals.tf
+++ tofu/modules/sebt_ses/locals.tf
@@ -1,6 +1,4 @@
 locals {
-  prefix        = "${var.project}-${var.environment}"
-  project_short = var.project_short != "" ? var.project_short : var.project
-  short_prefix  = "${local.project_short}-${var.environment}"
-  smtp_server   = "email-smtp.${data.aws_region.current.name}.${data.aws_partition.current.dns_suffix}"
+  prefix      = "${var.project}-${var.environment}"
+  smtp_server = "email-smtp.${data.aws_region.current.name}.${data.aws_partition.current.dns_suffix}"
 }
--- tofu/modules/sebt_ses/variables.tf
+++ tofu/modules/sebt_ses/variables.tf
@@ -9,14 +9,7 @@ variable "project" {
   description = "Project that these resources are supporting."
 }
 
-variable "project_short" {
-  type        = string
-  description = "Abbreviated project name for resource naming."
-  default     = ""
-}
-
 variable "sender_email" {
   type        = string
   description = "Email address used as the sender for outgoing emails. Must be verified in SES."
 }
-
