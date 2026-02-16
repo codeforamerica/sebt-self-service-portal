5ac44d09388d7114064345ea599b6a2253639a9d
spokenbird <agonzalez@codeforamerica.org>
2026-02-16 14:46:52 -0800

    Add IAM user and ECR repositories to bootstrap config

--- tofu/config/bootstrap/main.tf
+++ tofu/config/bootstrap/main.tf
@@ -4,3 +4,36 @@ module "backend" {
   project     = var.project
   environment = var.environment
 }
+
+# IAM user for GitHub Actions CI/CD.
+resource "aws_iam_user" "github_actions" {
+  name = "${var.project}-${var.environment}-github-actions"
+}
+
+resource "aws_iam_user_policy_attachment" "github_actions" {
+  user       = aws_iam_user.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_iam_access_key" "github_actions" {
+  user = aws_iam_user.github_actions.name
+}
+
+# ECR repositories for container images.
+resource "aws_ecr_repository" "api" {
+  name                 = "${var.project}-${var.environment}-api"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "web" {
+  name                 = "${var.project}-${var.environment}-web"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
