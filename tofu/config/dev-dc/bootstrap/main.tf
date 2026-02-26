63784c16306f029219fa2a4a3a055aa121b8bc3b
spokenbird <agonzalez@codeforamerica.org>
2026-02-26 14:42:34 -0800

    Update github IAM policy

--- tofu/config/dev-dc/bootstrap/main.tf
+++ tofu/config/dev-dc/bootstrap/main.tf
@@ -10,9 +10,47 @@ resource "aws_iam_user" "github_actions" {
   name = "${var.project}-${var.state}-${var.environment}-github-actions"
 }
 
+resource "aws_iam_policy" "github_actions" {
+  name        = "${var.project}-${var.state}-${var.environment}-github-actions"
+  description = "Service-scoped access for GitHub Actions CI/CD deployments."
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowDeploymentServices"
+        Effect = "Allow"
+        Action = [
+          "acm:*",
+          "cloudfront:*",
+          "dynamodb:*",
+          "ec2:*",
+          "ecr:*",
+          "ecs:*",
+          "elasticloadbalancing:*",
+          "iam:*",
+          "kms:*",
+          "lambda:*",
+          "logs:*",
+          "organizations:*",
+          "rds:*",
+          "route53:*",
+          "s3:*",
+          "secretsmanager:*",
+          "ses:*",
+          "ssm:*",
+          "sts:*",
+          "wafv2:*",
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
 resource "aws_iam_user_policy_attachment" "github_actions" {
   user       = aws_iam_user.github_actions.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+  policy_arn = aws_iam_policy.github_actions.arn
 }
 
 resource "aws_iam_access_key" "github_actions" {
