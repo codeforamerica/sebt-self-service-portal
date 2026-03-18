1ac435a055c29fc4cbb0f33da789d30347cb7571
Nick Campanini <ncampanini@codeforamerica.org>
2026-03-18 00:41:21 -0400

    Add card selection page with children checkboxes, CO card numbers, and validation

--- src/SEBT.Portal.Web/src/app/(authenticated)/profile/address/(flow)/replacement-cards/select/page.tsx
+++ src/SEBT.Portal.Web/src/app/(authenticated)/profile/address/(flow)/replacement-cards/select/page.tsx
@@ -2,14 +2,15 @@
 
 import { useTranslation } from 'react-i18next'
 
+import { CardSelection } from '@/features/address/components/CardSelection'
+
 export default function CardSelectionPage() {
   const { t } = useTranslation('confirmInfo')
 
   return (
     <div className="grid-container maxw-tablet">
       <h1>{t('cardSelectionTitle', 'Which cards need to be replaced?')}</h1>
-      <p>{t('cardSelectionIntro', 'Select the cards you would like to replace.')}</p>
-      {/* CardSelection component will be wired up in Step 6 */}
+      <CardSelection />
     </div>
   )
 }
