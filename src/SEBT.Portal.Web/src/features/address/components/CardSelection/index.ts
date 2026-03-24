52cb693bc67f30e45b64bdb8ad3b9a4c949ed19f
Nick Campanini <ncampanini@codeforamerica.org>
2026-03-23 21:33:35 -0400

    Move CardSelection component to features/cards

--- src/SEBT.Portal.Web/src/features/address/components/CardSelection/index.ts
+++ src/SEBT.Portal.Web/src/features/address/components/CardSelection/index.ts
@@ -1,1 +1,2 @@
-export { CardSelection } from './CardSelection'
+// Re-export from canonical location (moved to features/cards in DC-153 Step 0b)
+export { CardSelection } from '@/features/cards/components/CardSelection/CardSelection'
