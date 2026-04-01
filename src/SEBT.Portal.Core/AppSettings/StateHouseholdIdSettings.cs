8575649ab0f2819d28ce1a1699b0f2a675acc014
spokenbird <agonzalez@codeforamerica.org>
2026-04-01 14:22:30 -0700

    Use HashSet

--- src/SEBT.Portal.Core/AppSettings/StateHouseholdIdSettings.cs
+++ src/SEBT.Portal.Core/AppSettings/StateHouseholdIdSettings.cs
@@ -14,5 +14,5 @@ public class StateHouseholdIdSettings
     /// Ordered list of household ID types for authorization/linking.
     /// The first type that can be resolved from the user is used for lookup.
     /// </summary>
-    public List<PreferredHouseholdIdType> PreferredHouseholdIdTypes { get; set; } = [];
+    public HashSet<PreferredHouseholdIdType> PreferredHouseholdIdTypes { get; set; } = [];
 }
