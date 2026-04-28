4b4e6f6e720c6523886ace89903f874429e3b179
James Blair <jblair@codeforamerica.org>
2026-04-28 12:52:18 -0600

    minor refactor in OpenTelemetrySetup.cs

--- src/SEBT.Portal.Api/Telemetry/OpenTelemetrySetup.cs
+++ src/SEBT.Portal.Api/Telemetry/OpenTelemetrySetup.cs
@@ -11,11 +11,13 @@ internal static class OpenTelemetrySetup
 
     public static void SetupOpenTelemetry(this WebApplicationBuilder builder)
     {
-        // TODO - Unsure we want to actually register this because we're 
-        // using Serilog structured logging. If needed, I think there is
-        // a Serilog log-sink package for OTEL to adapt it. Primarily we 
-        // want tracing and metrics for now.
-
+        /* 
+         * OTEL Logging is supported by .NET packages, but we are not currently
+         * going to set this up. We're using Serilog for semantic logging, which
+         * writes to stdout -> cloudwatch -> etc.
+         *
+         * If we ever move to OTEL-based logging, we'll set it up here.
+         */
         // builder.Logging.AddOpenTelemetry(options =>
         // {
         //     options.SetResourceBuilder(
@@ -25,87 +27,91 @@ internal static class OpenTelemetrySetup
 
         var configSection = builder.Configuration.GetSection("Otel");
 
+        // Use IConfiguration binding for AspNetCore instrumentation options.
+        builder.Services.Configure<AspNetCoreTraceInstrumentationOptions>(configSection.GetSection("AspNetCoreInstrumentation"));
+
         builder.Services.AddOpenTelemetry()
             .ConfigureResource(r => r.AddService(
                 serviceName: ServiceName,
                 serviceInstanceId: Environment.MachineName
             // TODO serviceVersion: ???
             ))
-            .WithTracing(tracingBuilder =>
-            {
-                tracingBuilder
-                    .AddSource(InstrumentationSource.ActivitySourceName)
-                    .SetSampler(new AlwaysOnSampler())
-                    .AddHttpClientInstrumentation()
-                    .AddAspNetCoreInstrumentation();
-
-                // Use IConfiguration binding for AspNetCore instrumentation options.
-                builder.Services.Configure<AspNetCoreTraceInstrumentationOptions>(configSection.GetSection("AspNetCoreInstrumentation"));
-
-                // Note: Switch between OTLP/Console by setting UseTracingExporter in appsettings.json.
-                var tracingExporter = configSection.GetValue("UseTracingExporter", defaultValue: "CONSOLE").ToUpperInvariant();
-
-                switch (tracingExporter)
-                {
-                    case "OTLP":
-                        tracingBuilder.AddOtlpExporter(otlpOptions =>
-                        {
-                            // Use IConfiguration directly for Otlp exporter endpoint option.
-                            otlpOptions.Endpoint = new Uri(configSection.GetValue("Otlp:Endpoint", defaultValue: "http://localhost:4317"));
-                        });
-                        break;
-
-                    default:
-                        tracingBuilder.AddConsoleExporter();
-                        break;
-                }
-            })
-            .WithMetrics(metricsBuilder =>
-            {
-                metricsBuilder
-                    .AddMeter(InstrumentationSource.MeterName)
-                    .SetExemplarFilter(ExemplarFilterType.TraceBased)
-                    .AddRuntimeInstrumentation()
-                    .AddHttpClientInstrumentation()
-                    .AddAspNetCoreInstrumentation();
-
-                // Note: Switch between Explicit/Exponential by setting HistogramAggregation in appsettings.json
-                var histogramAggregation = configSection.GetValue("HistogramAggregation", defaultValue: "EXPLICIT").ToUpperInvariant();
-
-                switch (histogramAggregation)
-                {
-                    case "EXPONENTIAL":
-                        metricsBuilder.AddView(instrument =>
-                        {
-                            return instrument.GetType().GetGenericTypeDefinition() == typeof(Histogram<>)
-                                ? new Base2ExponentialBucketHistogramConfiguration()
-                                : null;
-                        });
-                        break;
-                    default:
-                        // Explicit bounds histogram is the default.
-                        // No additional configuration necessary.
-                        break;
-                }
-
-                // Note: Switch between Prometheus/OTLP/Console by setting UseMetricsExporter in appsettings.json.
-                var metricsExporter = configSection.GetValue("UseMetricsExporter", defaultValue: "CONSOLE").ToUpperInvariant();
-
-                switch (metricsExporter)
-                {
-                    case "OTLP":
-                        metricsBuilder.AddOtlpExporter(otlpOptions =>
-                        {
-                            // Use IConfiguration directly for Otlp exporter endpoint option.
-                            otlpOptions.Endpoint = new Uri(configSection.GetValue("Otlp:Endpoint", defaultValue: "http://localhost:4317"));
-                        });
-                        break;
-                    default:
-                        metricsBuilder.AddConsoleExporter();
-                        break;
-                }
-            });
+            .WithTracing(tracingBuilder => ConfigureTracing(tracingBuilder, configSection))
+            .WithMetrics(metricsBuilder => ConfigureMetrics(metricsBuilder, configSection));
 
         builder.Services.AddSingleton<IInstrumentationSource, InstrumentationSource>();
+    }
+
+    private static void ConfigureTracing(TracerProviderBuilder tracingBuilder, IConfiguration configSection)
+    {
+        tracingBuilder
+            .AddSource(InstrumentationSource.ActivitySourceName)
+            .SetSampler(new AlwaysOnSampler())
+            .AddHttpClientInstrumentation()
+            .AddAspNetCoreInstrumentation();
+
+        // Note: Switch between OTLP/Console by setting UseTracingExporter in appsettings.json.
+        var tracingExporter = configSection.GetValue("UseTracingExporter", defaultValue: "CONSOLE").ToUpperInvariant();
+
+        switch (tracingExporter)
+        {
+            case "OTLP":
+                tracingBuilder.AddOtlpExporter(otlpOptions =>
+                {
+                    // Use IConfiguration directly for Otlp exporter endpoint option.
+                    otlpOptions.Endpoint = new Uri(configSection.GetValue("Otlp:Endpoint", defaultValue: "http://localhost:4317"));
+                });
+                break;
+
+            default:
+                tracingBuilder.AddConsoleExporter();
+                break;
+        }
+    }
+
+    private static void ConfigureMetrics(MeterProviderBuilder metricsBuilder, IConfigurationSection configSection)
+    {
+        metricsBuilder
+            .AddMeter(InstrumentationSource.MeterName)
+            .SetExemplarFilter(ExemplarFilterType.TraceBased)
+            .AddRuntimeInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddAspNetCoreInstrumentation();
+
+        // Note: Switch between Explicit/Exponential by setting HistogramAggregation in appsettings.json
+        var histogramAggregation = configSection.GetValue("HistogramAggregation", defaultValue: "EXPLICIT").ToUpperInvariant();
+
+        switch (histogramAggregation)
+        {
+            case "EXPONENTIAL":
+                metricsBuilder.AddView(instrument =>
+                {
+                    return instrument.GetType().GetGenericTypeDefinition() == typeof(Histogram<>)
+                        ? new Base2ExponentialBucketHistogramConfiguration()
+                        : null;
+                });
+                break;
+            default:
+                // Explicit bounds histogram is the default.
+                // No additional configuration necessary.
+                break;
+        }
+
+        // Note: Switch between Prometheus/OTLP/Console by setting UseMetricsExporter in appsettings.json.
+        var metricsExporter = configSection.GetValue("UseMetricsExporter", defaultValue: "CONSOLE").ToUpperInvariant();
+
+        switch (metricsExporter)
+        {
+            case "OTLP":
+                metricsBuilder.AddOtlpExporter(otlpOptions =>
+                {
+                    // Use IConfiguration directly for Otlp exporter endpoint option.
+                    otlpOptions.Endpoint = new Uri(configSection.GetValue("Otlp:Endpoint", defaultValue: "http://localhost:4317"));
+                });
+                break;
+            default:
+                metricsBuilder.AddConsoleExporter();
+                break;
+        }
     }
 }
