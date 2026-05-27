6cffc717bfa62719791165ee97ad3e325c1d80c4
Peter Karman <peter.karman@state.co.us>
2026-05-27 10:28:47 -0500

    quote js snip

--- src/SEBT.EnrollmentChecker.Web/src/app/layout.tsx
+++ src/SEBT.EnrollmentChecker.Web/src/app/layout.tsx
@@ -38,14 +38,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <meta name="build-sha" content={process.env.NEXT_PUBLIC_BUILD_SHA} />
         )}
         {process.env.NEXT_PUBLIC_META_PIXEL && (
-           <script>
+           <script>{`
            !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
            n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
            n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
            t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
            document,'script','https://connect.facebook.net/en_US/fbevents.js');
            fbq('init', '{process.env.NEXT_PUBLIC_META_PIXEL}');
-           fbq('track', "PageView");</script>
+           fbq('track', "PageView");`}</script>
            <noscript><img height="1" width="1" style="display:none"
            src="https://www.facebook.com/tr?id={process.env.NEXT_PUBLIC_META_PIXEL}&ev=PageView&noscript=1"
            /></noscript>
