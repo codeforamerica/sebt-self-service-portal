a33a9c4b8131eba104a0584dce3276cdc00e504f
Peter Karman <peter.karman@state.co.us>
2026-05-27 10:42:59 -0500

    interpolate

--- src/SEBT.EnrollmentChecker.Web/src/app/layout.tsx
+++ src/SEBT.EnrollmentChecker.Web/src/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
            n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
            t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
            document,'script','https://connect.facebook.net/en_US/fbevents.js');
-           fbq('init', '{process.env.NEXT_PUBLIC_META_PIXEL}');
+           fbq('init', '${process.env.NEXT_PUBLIC_META_PIXEL}');
            fbq('track', "PageView");`}</script>
         )}
       </head>
