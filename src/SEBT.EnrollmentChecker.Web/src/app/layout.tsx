baa8a76f4e222586bc452b2afaa625fac53df818
Peter Karman <peter.karman@state.co.us>
2026-05-28 10:28:42 -0500

    CSP via CF

--- src/SEBT.EnrollmentChecker.Web/src/app/layout.tsx
+++ src/SEBT.EnrollmentChecker.Web/src/app/layout.tsx
@@ -45,9 +45,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <meta name="build-sha" content={process.env.NEXT_PUBLIC_BUILD_SHA} />
         )}
         {process.env.NEXT_PUBLIC_META_PIXEL && (
-           <meta http-equiv="Content-Security-Policy" content="script-src 'self' https://connect.facebook.net; connect-src 'self' https://graph.facebook.com;" />
-        )}
-        {process.env.NEXT_PUBLIC_META_PIXEL && (
            <Script src={`${process.env.NEXT_PUBLIC_BASE_PATH}/meta_pixel.js`} />
         )}
       </head>
