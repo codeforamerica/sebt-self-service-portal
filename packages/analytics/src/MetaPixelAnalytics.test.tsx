9cefe2e948ce5c0727959b8ec8082372c17bbeda
Peter Karman <peter.karman@state.co.us>
2026-07-17 16:42:05 -0500

    refactor to inline everything

--- packages/analytics/src/MetaPixelAnalytics.test.tsx
+++ packages/analytics/src/MetaPixelAnalytics.test.tsx
@@ -1,10 +1,6 @@
 import { render } from '@testing-library/react'
 import { afterEach, describe, expect, it, vi } from 'vitest'
 
-const { fbq } = vi.hoisted(() => ({
-  fbq: vi.fn(() => () => {})
-}))
-
 // Capture props from both Script elements so we can assert the wiring without
 // making real network requests or executing the boot snippet in jsdom.
 type ScriptRecord = { id?: string; src?: string; html?: string; onReady?: () => void }
@@ -30,28 +26,15 @@ import { MetaPixelAnalytics } from './MetaPixelAnalytics'
 
 afterEach(() => {
   scripts.length = 0
-  vi.unstubAllGlobals()
 })
 
 describe('MetaPixelAnalytics', () => {
   it('inlines the stub snippet so window.fbq() is defined', () => {
     render(<MetaPixelAnalytics pixelId="test-pixel" />)
+
     const stub = scripts.find((s) => s.id === 'meta-pixel-stub')
 
     expect(stub?.html).toContain('connect.facebook.net')
-  })
-
-  it('calls fbq() with the provided pixel id when ready', () => {
-    // Object.defineProperty(window, 'fbq', vi.fn().mockImplementation((event, eventValue) => ({ })));
-
-    render(<MetaPixelAnalytics pixelId="test-pixel" />)
-
-    const cdn = scripts.find((s) => s.id === 'meta-pixel-stub')
-
-    window.fbq = function(event, eventValue) { console.log(`${event} ${eventValue}`) }
-
-    cdn?.onReady?.()
-
-    expect(fbq).toHaveBeenCalledWith('test-pixel')
+    expect(stub?.html).toContain("fbq('init','test-pixel')")
   })
 })
--- packages/analytics/src/MetaPixelAnalytics.tsx
+++ packages/analytics/src/MetaPixelAnalytics.tsx
@@ -6,20 +6,18 @@ interface MetaPixelAnalyticsProps {
   pixelId: string
 }
 
-const META_PIXEL_STUB_SNIPPET = `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');`
+const META_PIXEL_STUB_SNIPPET = `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js')`
 
 export function MetaPixelAnalytics({ pixelId }: MetaPixelAnalyticsProps) {
+  const metaJs = `${META_PIXEL_STUB_SNIPPET};fbq('init','${pixelId}');fbq('track','PageView');`
+
   return (
     <>
       {/* should be loaded inside <head> right before closing </head> tag */}
       <Script
         id="meta-pixel-stub"
-        dangerouslySetInnerHTML={{ __html: META_PIXEL_STUB_SNIPPET }}
+        dangerouslySetInnerHTML={{ __html: metaJs }}
         strategy="afterInteractive"
-        onReady={() => {
-          (window as any).fbq('init', pixelId)
-          (window as any).fbq('track', 'PageView')
-        }}
       />
     </>
   )
