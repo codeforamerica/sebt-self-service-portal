15a59552688db04d4d1ae39d0256216588a95600
Peter Karman <peter.karman@state.co.us>
2026-07-21 16:21:27 -0500

    fbq typescript def

--- packages/analytics/src/data-layer.ts
+++ packages/analytics/src/data-layer.ts
@@ -368,5 +368,13 @@ export class DataLayer {
 declare global {
   interface Window {
     digitalData?: DataLayerRoot
+    fbq: {
+      (...args: any[]): void;
+      push: (...args: any[]) => void;
+      loaded: boolean;
+      version: string;
+      queue: any[];
+      disablePushState?: boolean;
+    };
   }
 }
