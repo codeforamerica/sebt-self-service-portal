4a46f3438aa9de83f0f2d6afc620d9252a55395f
Peter Karman <peter.karman@state.co.us>
2026-07-22 14:42:34 -0500

    add missing import

--- apps/portal/src/SEBT.EnrollmentChecker.Web/src/features/enrollment/components/DisclaimerPage.tsx
+++ apps/portal/src/SEBT.EnrollmentChecker.Web/src/features/enrollment/components/DisclaimerPage.tsx
@@ -4,6 +4,7 @@ import { Button } from '@sebt/design-system'
 import { useRouter } from 'next/navigation'
 import { useTranslation } from 'react-i18next'
 import { useEffect, useState } from 'react'
+import { AdentifiPixels } from '@sebt/analytics'
 
 export function DisclaimerPage() {
   const { t } = useTranslation('disclaimer')
