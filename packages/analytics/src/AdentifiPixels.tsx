496b86a9176672434f6b32906e013435e141334f
Peter Karman <peter.karman@state.co.us>
2026-07-22 15:38:20 -0500

    remove href param, fix lint

--- packages/analytics/src/AdentifiPixels.tsx
+++ packages/analytics/src/AdentifiPixels.tsx
@@ -4,23 +4,24 @@ import { useState, useMemo } from "react";
 
 interface AdentifiPixelsProps {
   pixelId: string
-  href: string
 }
 
-export function AdentifiPixels({ pixelId, href }: AdentifiPixelsProps) {
-  const [nonce, setNonce] = useState("");
+export function AdentifiPixels({ pixelId }: AdentifiPixelsProps) {
+  const [nonce, setNonce] = useState("")
+  const [href, setHref] = useState("")
 
   useMemo(() => {
     const r = Buffer.from(crypto.randomUUID()).toString('base64')
     
-    setNonce(r);
+    setNonce(r)
+    setHref(window.location.href)
   }, []) // Empty array ensures this only runs once when the component mounts
 
   const url = encodeURIComponent(href)
   const src = `https://px.adentifi.com/Pixels?a_id=${pixelId};p_url=${url};uq=${nonce}`
 
   // on initial load href may be blank
-  if (!href) {
+  if (!window) {
     return <></>
   }
 
